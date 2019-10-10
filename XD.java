87a05df76d88109709d0bd38e55c19b37a8821d7
theDuro <52936119+theDuro@users.noreply.github.com>
2019-10-10 14:26:22 +0200

    add filles

---
+++ XD.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xd;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class XD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    public int[][] hp(int n){
+      int  hh[][] = new int[n][n];
+      for(int i =0; i<n;i++){
+      
+      
+      
+      
+      }
+     return hh;   
+    }
+}
